87a94b9a4d45b982278be931e7d5242e0607f976
Oleh Klepatskyi <o.klepatskyi@gmail.com>
2024-03-19 21:28:18 +0200

    Add io functions declaration

---
+++ app/io/input.py
@@ -0,0 +1,9 @@
+def get_user_input(prompt : str = "Enter your text: ") -> str:
+    pass
+
+def read_file(filename: str) -> str:
+    pass
+
+
+def read_file_with_pandas(filename: str) -> str:
+    pass
---
+++ app/io/output.py
@@ -0,0 +1,5 @@
+def print_to_console(text: str):
+    pass
+
+def write_to_file(text : str, filename : str):
+    pass
